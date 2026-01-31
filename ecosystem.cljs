1f26dd2efc4a4a539cf8c833334a1d4e76293372
Error <foamy125@gmail.com>
2026-01-31 13:05:45 -0600

    refactor(ecosystem): move gates-of-aker ecosystem to cljs format

--- ecosystem.cljs
+++ ecosystem.cljs
@@ -1,36 +1,38 @@
-(load-file "pm2-clj-project/src/clobber/macro.cljs")
+ (ns gates-of-aker.ecosystem)
 
-(clobber.macro/defapp "gates-backend"
-  {:script "clojure"
-   :cwd "./backend"
-   :args ["-M:server"]
-   :interpreter "bash"
-   :instances 1
-   :autorestart true
-   :watch ["src" "resources" "dev" "deps.edn"]
-   :ignore_watch ["target" "logs"]
-   :watch_delay 1000
-   :log_file "./logs/backend.log"
-   :out_file "./logs/backend-out.log"
-   :error_file "./logs/backend-error.log"
-   :log_date_format "YYYY-MM-DD HH:mm:ss Z"
-   :env {:PORT 3000
-         :NODE_ENV "development"}})
+ ;; Backend processes
+ (defapp "gates-backend"
+   {:script "clojure"
+    :cwd "./backend"
+    :args ["-M:server"]
+    :interpreter "bash"
+    :instances 1
+    :autorestart true
+    :watch ["src" "resources" "dev" "deps.edn"]
+    :ignore_watch ["target" "logs"]
+    :watch_delay 1000
+    :log_file "./logs/backend.log"
+    :out_file "./logs/backend-out.log"
+    :error_file "./logs/backend-error.log"
+    :log_date_format "YYYY-MM-DD HH:mm:ss Z"
+    :env {:PORT 3000
+          :NODE_ENV "development"}})
 
-(clobber.macro/defapp "gates-frontend"
-  {:cwd "./web"
-   :script "npm"
-   :args ["run" "dev"]
-   :interpreter "node"
-   :instances 1
-   :autorestart true
-   :watch ["src" "index.html"]
-   :watch_delay 1000
-   :log_file "../backend/logs/frontend.log"
-   :out_file "../backend/logs/frontend-out.log"
-   :error_file "../backend/logs/frontend-error.log"
-   :log_date_format "YYYY-MM-DD HH:mm:ss Z"
-   :env {:PORT 5173
-         :NODE_ENV "development"}})
+ (defapp "gates-frontend"
+   {:cwd "./web"
+    :script "npm"
+    :args ["run" "dev"]
+    :interpreter "node"
+    :instances 1
+    :autorestart true
+    :watch ["src" "index.html"]
+    :watch_delay 1000
+    :log_file "../backend/logs/frontend.log"
+    :out_file "../backend/logs/frontend-out.log"
+    :error_file "../backend/logs/frontend-error.log"
+    :log_date_format "YYYY-MM-DD HH:mm:ss Z"
+    :env {:PORT 5173
+          :NODE_ENV "development"}})
 
-(clobber.macro/ecosystem)
+;; Run ecosystem post-setup script
+ (clobber.macro/ecosystem-output)
